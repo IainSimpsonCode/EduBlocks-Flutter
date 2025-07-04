87d7c717f944864af660855e9da372c29eba51c2
Senan192 <senanyatigammana@gmail.com>
2025-07-04 17:24:27 +0100

    pt2

--- edublocks_flutter/lib/Widgets/categoryScroller.dart
+++ edublocks_flutter/lib/Widgets/categoryScroller.dart
@@ -1,9 +1,8 @@
 import 'package:edublocks_flutter/Services/providers.dart';
 import 'package:edublocks_flutter/style.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 
 class categoryScroller extends StatefulWidget {
   const categoryScroller({super.key});
@@ -13,102 +12,138 @@ class categoryScroller extends StatefulWidget {
 }
 
 class _categoryScrollerState extends State<categoryScroller> {
-  @override
   int? selectedIndex;
 
+  @override
   Widget build(BuildContext context) {
     final categories =
         Provider.of<BlockLibrary>(context, listen: false).categories;
     final isCategorySelected =
         Provider.of<BlockLibrary>(context).hasCategorySelected();
 
-    return ListView.builder(
-      itemCount: categories.length,
-      itemBuilder: (context, index) {
-        final category = categories[index];
-        bool isHovered = false;
-        final isSelected = selectedIndex == index;
+    return LayoutBuilder(
+      // ‹constraints› reflects the width actually allotted to this sidebar.
+      builder: (context, constraints) {
+        // Reveal text only if there’s enough room for icon + gap + ~90 px of text.
+        final bool showText = constraints.maxWidth > 75;
+        final double maxWidth = constraints.maxWidth;
 
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return MouseRegion(
-              onEnter: (_) => setState(() => isHovered = true),
-              onExit: (_) => setState(() => isHovered = false),
-              child: AnimatedPadding(
-                duration: const Duration(milliseconds: 200),
-                padding: EdgeInsets.all(
-                  isHovered || (isSelected && isCategorySelected) ? 3.0 : 0.0,
-                ),
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedIndex = index;
-                    });
-                    Provider.of<BlockLibrary>(
-                      context,
-                      listen: false,
-                    ).setCategorySelected(category.category);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color:
-                          isSelected && isCategorySelected
-                              ? category.color.withOpacity(1)
-                              : (isHovered ? buttonGreyColour : null),
-                      borderRadius: BorderRadius.circular(8),
+        // Shrink icon size at smaller widths
+        double iconSize;
+        if (maxWidth > 60) {
+          iconSize = 32;
+        } else if (maxWidth > 25) {
+          iconSize = 24;
+        } else {
+          iconSize = 20;
+        }
+        return ListView.builder(
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            final category = categories[index];
+            final bool isSelected = selectedIndex == index;
+            bool isHovered = false;
+
+            return StatefulBuilder(
+              builder: (context, setState) {
+                return MouseRegion(
+                  onEnter: (_) => setState(() => isHovered = true),
+                  onExit: (_) => setState(() => isHovered = false),
+                  child: AnimatedPadding(
+                    duration: const Duration(milliseconds: 200),
+                    padding: EdgeInsets.all(
+                      isHovered || (isSelected && isCategorySelected)
+                          ? 3.0
+                          : 0.0,
                     ),
-                    child: ListTile(
-                      leading: Container(
-                        width: 32,
-                        height: 32,
-                        margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() => selectedIndex = index);
+                        Provider.of<BlockLibrary>(
+                          context,
+                          listen: false,
+                        ).setCategorySelected(category.category);
+                      },
+                      child: Container(
                         decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.25),
-                              blurRadius: 6,
-                              offset: const Offset(0, 3),
-                            ),
-                          ],
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: category.color,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          padding: const EdgeInsets.all(6),
-                          child: SvgPicture.asset(
-                            'app_assets/category_icons/${category.iconName}.svg',
-                            color: Colors.white,
-                            fit: BoxFit.contain,
-                            errorBuilder: (context, error, stackTrace) {
-                              return const Icon(Icons.broken_image, size: 80);
-                            },
-                          ),
-                        ),
-                      ),
-                      title: Text(
-                        category.category,
-                        style: bodyMedium.copyWith(
                           color:
                               isSelected && isCategorySelected
-                                  ? Colors.white
-                                  : buttonTextColour,
-                          fontWeight:
-                              isSelected && isCategorySelected
-                                  ? FontWeight.bold
-                                  : FontWeight.normal,
+                                  ? category.color
+                                  : (isHovered ? buttonGreyColour : null),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 12.0,
+                          vertical: 8.0,
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            _buildIcon(category, iconSize),
+                            if (showText) ...[
+                              const SizedBox(width: 12),
+                              Expanded(
+                                child: Text(
+                                  category.category,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: bodyMedium.copyWith(
+                                    color:
+                                        isSelected && isCategorySelected
+                                            ? Colors.white
+                                            : buttonTextColour,
+                                    fontWeight:
+                                        isSelected && isCategorySelected
+                                            ? FontWeight.bold
+                                            : FontWeight.normal,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ],
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
+                );
+              },
             );
           },
         );
       },
+    );
+  }
+
+  /// Builds the coloured square icon for a category.
+  Widget _buildIcon(category, double size) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.25),
+            blurRadius: 6,
+            offset: const Offset(0, 3),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: category.color,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.all(4),
+        child: SvgPicture.asset(
+          'app_assets/category_icons/${category.iconName}.svg',
+          color: Colors.white,
+          fit: BoxFit.contain,
+          width: size * 0.6,
+          height: size * 0.6,
+          errorBuilder:
+              (_, __, ___) => Icon(Icons.broken_image, size: size * 0.6),
+        ),
+      ),
     );
   }
 }
