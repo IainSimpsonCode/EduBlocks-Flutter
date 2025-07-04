a7fdf7728498b59fd294bd33e01dbd30390f85c7
Iain Simpson <147500777+IainSimpsonCode@users.noreply.github.com>
2025-07-04 15:36:59 +0100

    Fixed logging

--- Node Server/index.js
+++ Node Server/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const axios = require("axios");
 const path = require("path");
 const app = express()
 
@@ -19,21 +18,6 @@ app.get("/", (req, res) => {
 app.get("/v1", (req, res) => {
   res.sendFile(path.join(flutterWebAppPath, "index.html"));
 })
-
-app.post('/log', async (req, res) => {
-  const targetURL = 'https://marklochrie.co.uk/edublocks_logger/log';
-
-  try {
-    const response = await axios.post(targetURL, req.body, {
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    res.status(200).json({ message: 'Data forwarded successfully' });
-  } catch (error) {
-    console.error('Error forwarding data:', error.message);
-    res.status(500).json({ error: 'Failed to forward data' });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}\nhttp://localhost:${PORT}`);
