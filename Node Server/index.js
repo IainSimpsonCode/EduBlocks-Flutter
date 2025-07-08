ed527fad318b4ad54a3746a180dc3d02bda13b9c
Iain Simpson <147500777+IainSimpsonCode@users.noreply.github.com>
2025-07-08 08:59:57 +0100

    Added /live to node server

--- Node Server/index.js
+++ Node Server/index.js
@@ -4,12 +4,13 @@ const app = express()
 
 const PORT = 3001;
 
-//const flutterWebAppPath = path.join(__dirname, '..', "edublocks_flutter", "build", "web");
-const flutterWebAppPath = path.join(__dirname, "V2");
+const flutterWebAppPath = path.join(__dirname, '..', "edublocks_flutter", "build", "web");
+const v2WebAppPath = path.join(__dirname, "V2");
 const v1WebAppPath = path.join(__dirname, "V1");
 
-app.use('/v2', express.static(flutterWebAppPath));
+app.use('/v2', express.static(v2WebAppPath));
 app.use('/v1', express.static(v1WebAppPath));
+app.use('/live', express.static(flutterWebAppPath));
 
 app.get("/", (req, res) => {
   res.redirect('/v2');
@@ -20,6 +21,10 @@ app.get("/v1", (req, res) => {
 })
 
 app.get("/v2", (req, res) => {
+  res.sendFile(path.join(v2WebAppPath, "index.html"));
+})
+
+app.get("/live", (req, res) => {
   res.sendFile(path.join(flutterWebAppPath, "index.html"));
 })
 
